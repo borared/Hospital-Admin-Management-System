3bcc20b6f5484cd6b6d13d35b83cc68d3e6ecc13
borared <bora.red@student.cadt.edu.kh>
2026-01-31 22:30:46 +0700

    Add appointment

---
+++ src/main/java/adminmangementsystem/com/Appointment.java
@@ -0,0 +1,10 @@
+package adminmangementsystem.com;
+
+public class Appointment {
+    private String patientId;
+    private String patientName;
+    private String disease;
+    private String appointmentDate;
+
+    
+}
