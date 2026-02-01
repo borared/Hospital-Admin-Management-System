1400574109ec4a0c4d9e7d4ff07121627155bc89
borared <bora.red@student.cadt.edu.kh>
2026-02-01 10:15:29 +0700

    Update

--- src/main/java/adminmangementsystem/com/App.java
+++ src/main/java/adminmangementsystem/com/App.java
@@ -2,9 +2,6 @@ package adminmangementsystem.com;
 
 import java.util.Scanner;
 
-
-import java.util.Scanner;
-
 public class App {
  public static void main(String[] args) {
     System.out.println("Hello World!");
