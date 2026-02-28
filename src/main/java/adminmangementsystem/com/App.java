c5bcb7fbcb8a2ac7c8dc48d327fc85937c768962
borared <boraofficial76@gmail.com>
2026-02-28 21:42:42 +0700

    Updated App.java to respect from MainController.java in controller folder

--- src/main/java/adminmangementsystem/com/App.java
+++ src/main/java/adminmangementsystem/com/App.java
@@ -16,6 +16,11 @@ public class App {
         PatientSystem patientSystem = new PatientSystem();
         DoctorSystem doctorSystem = new DoctorSystem();
 
+        DoctorController doctorController = new DoctorController(doctorSystem);
+        PatientController patientController = new PatientController(patientSystem);
+        AppointmentController appointmentController = new AppointmentController(appointmentSystem);
+        MainController mainController = new MainController(doctorController, patientController, appointmentController);
+
         boolean loggedIn = false;
 
         while (!loggedIn) {
@@ -50,157 +55,8 @@ public class App {
             }
         }
 
-        boolean running = true;
-
-        while (running) {
-            Menu.printDashboardMenu();
-            System.out.print("Enter your choice: ");
-            int choiceIn = sc.nextInt();
-            sc.nextLine(); 
-
-            switch (choiceIn) {
-                case 1: //Doctor System
-                    boolean doctorMenu = true;
-                    while(doctorMenu){
-                        Menu.doctorMenu();
-
-                        System.out.print("Choose an option: ");
-                        int doctorChoice = Integer.parseInt(sc.nextLine());
-
-                        switch (doctorChoice) {
-                            //Add Doctor
-                            case 1:
-                                doctorSystem.addDoctor(sc);
-                                break;
-                            //Update Doctor
-                            case 2:
-                                doctorSystem.updateDoctor(sc);
-                                break;
-                            //Delete Doctor    
-                            case 3:
-                                doctorSystem.deleteDoctor(sc);
-                                break;
-
-                            //Search Doctor
-                            case 4:
-                                doctorSystem.searchDoctor(sc);
-                                break;
-                            
-                            //View Doctor List
-                            case 5:
-                                doctorSystem.viewDoctorList();
-                                break;
-
-                            //Exit 
-                            case 6:
-                                doctorMenu = false;
-                                break;
-                        
-                            default:
-                                System.out.println("Invalid choice!");
-                                break;
-                        }
-                    }
-                    break;
-
-                case 2: // Patient Management System
-                    boolean patientMenu = true;
-                    while (patientMenu) {
-                        Menu.patientMenu();
-                        
-                        System.out.print("Choose an option: ");
-                        int patientChoice = Integer.parseInt(sc.nextLine());
-
-                        switch (patientChoice) {
-                            
-                            // --------- ADD PATIENT ----------
-                            case 1:
-                                patientSystem.addPatient(sc);
-                                break;
-
-                            // --------- UPDATE PATIENT ----------
-                           case 2: // Update Patient ID
-                                patientSystem.updatePatient(sc);
-                                break;
-
-                            // --------- DELETE PATIENT ----------
-                            case 3:
-                                patientSystem.deletePatient(sc);
-                                break;
-
-
-                            // --------- SEARCH PATIENT ----------
-                            case 4:
-                                patientSystem.searchPatient(sc);
-                                break;
-
-                            // --------- EXIT PATIENT MENU ----------
-                            case 5:
-                                patientMenu = false;
-                                System.out.println("Exiting Patient Management System...");
-                                break;
-
-                            default:
-                                System.out.println("Invalid choice! Please try again.");
-                                break;
-                        }
-                    }
-                break;
-
-
-                case 3: //Make an Appointment
-                    boolean appointmentDisplayMenu = true;
-                    
-                    while(appointmentDisplayMenu){
-
-                        Menu.appointmentMenu();
-
-                        System.out.print("Choose an option: ");
-                        int appointmentChoice;
-                        try {
-                            appointmentChoice = Integer.parseInt(sc.nextLine());
-                        } catch (Exception e) {
-                        System.out.println("Invalid choice!");
-                        continue;
-                        }
-                        switch (appointmentChoice){
-                            
-                            case 1:
-                                appointmentSystem.addAppointment(sc);
-                                break;
-                            case 2:
-                                appointmentSystem.viewAppointments();
-                                break;
-                            case 3:
-                                appointmentDisplayMenu = false;
-                            break;
-                            default:
-                                System.out.println("Invalid choice!");
-                        }
-                    }
-                    break;
-
-                case 4: // View Appointment List
-                    appointmentSystem.viewAppointments();
-                  break;
-
-                case 5: // View Doctor List
-                    doctorSystem.viewDoctorList1();
-                 break;
-
-                case 6: // View Patient List
-                    patientSystem.viewPatientList(sc);
-                  break;
-
-                case 7: // Exit dashboard
-                    running = false;
-                    System.out.println("Logged out.");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice.");
-            }
-        }
+        // hand over control to main controller
+        mainController.start(sc);
 
         sc.close();
     }
