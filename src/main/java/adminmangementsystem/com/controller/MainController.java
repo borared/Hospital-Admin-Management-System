f1f862827537701d4be3579ddf6f77051484df69
borared <boraofficial76@gmail.com>
2026-02-28 21:40:09 +0700

    I created Controller folder to handle Scanner sc from user and UI choice

---
+++ src/main/java/adminmangementsystem/com/controller/MainController.java
@@ -0,0 +1,60 @@
+package adminmangementsystem.com.controller;
+
+import java.util.Scanner;
+import adminmangementsystem.com.Menu;
+
+public class MainController {
+    private final DoctorController doctorController;
+    private final PatientController patientController;
+    private final AppointmentController appointmentController;
+
+    public MainController(DoctorController doctorController,
+                          PatientController patientController,
+                          AppointmentController appointmentController) {
+        this.doctorController = doctorController;
+        this.patientController = patientController;
+        this.appointmentController = appointmentController;
+    }
+
+    public void start(Scanner sc) {
+        boolean running = true;
+        while (running) {
+            Menu.printDashboardMenu();
+            System.out.print("Enter your choice: ");
+            int choiceIn;
+            try {
+                choiceIn = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid choice.\n");
+                continue;
+            }
+
+            switch (choiceIn) {
+                case 1:
+                    doctorController.run(sc);
+                    break;
+                case 2:
+                    patientController.run(sc);
+                    break;
+                case 3:
+                    appointmentController.run(sc);
+                    break;
+                case 4:
+                    appointmentController.getAppointmentService().viewAppointments();
+                    break;
+                case 5:
+                    doctorController.getDoctorSystem().viewDoctorList1();
+                    break;
+                case 6:
+                    patientController.getPatientSystem().viewPatientList(sc);
+                    break;
+                case 7:
+                    running = false;
+                    System.out.println("Logged out.");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
