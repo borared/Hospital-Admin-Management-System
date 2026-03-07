d3576c3285a9cd0bb562fa25bc6a4c6aa1a8d0ca
Chendev <panhaseth.sophat@outlook.com>
2026-03-07 10:50:41 +0700

    Cardiologist View

---
+++ src/main/java/adminmangementsystem/com/view/CardiologistView.java
@@ -0,0 +1,24 @@
+package adminmangementsystem.com.view;
+
+import java.util.Scanner;
+import adminmangementsystem.com.Validator;
+import adminmangementsystem.com.Cardiologist;
+
+public class CardiologistView {
+
+    public static Cardiologist getCardiologistInput(Scanner sc) {
+        System.out.println("\t\t------Add Cardiologist------");
+
+        String id = Validator.getValidPatient(sc, "Enter Cardiologist ID: ");
+        String name = Validator.getOnlyLetter(sc, "Enter name: ");
+        String dob = Validator.getValidDateFomart(sc, "Enter Date of Birth (dd/mm/yyyy): ");
+        String address = Validator.getNonEmpty(sc, "Enter address: ");
+        String email = Validator.getValidEmail(sc, "Enter email: ");
+        String position = Validator.getOnlyLetter(sc, "Enter position (e.g., Interventional): ");
+        double salary = Validator.getPositiveDouble(sc, "Enter salary: ");
+        String doe = Validator.getValidDateFomart(sc, "Enter date of entry (dd/mm/yyyy): ");
+        String fellowship = Validator.getNonEmpty(sc, "Enter fellowship: ");
+
+        return new Cardiologist(id, name, dob, address, email, position, salary, doe, fellowship);
+    }
+}
